999db3c71e4e8f0edbc92d0e1c77bb5bd1646546
AlessioMartella7 <alessio.mart87@gmail.com>
2024-08-30 17:41:22 +0200

    added fouls and point to teamData

--- js/snack4.js
+++ js/snack4.js
@@ -5,11 +5,21 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console */
 
+//creo una funzione per generare numeri random
+const getRandomNumber = (min, max) => (Math.floor(Math.random() * (max +1 -min)) + min);
+
 // creo un array di squadre di calcio
 
 const names = ['Giuventus','Torrino','Rioma', 'Lazie', 'Internos','Mailan']
 
 // creo una array con dentro i solo i nomi e le altre prop da compilare
 
-const teamData = names.map(name => ({name, points: 0, fouls: 0,}));
+const teamData = names.map(name => ({name, punti: 0, falli: 0,}));
+ 
+// aggiungiamo falli e punti al nostro array
+
+teamData.forEach(team => {
+    team.punti = getRandomNumber(3,90);
+    team.falli = getRandomNumber (10,100);
+})
 console.log(teamData)
